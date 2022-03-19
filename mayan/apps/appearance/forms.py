f9e44ef049d92e671dd34d21a71cce2352a1f5af
Onprawee <onprawee@kkumail.com>
2022-03-19 20:22:25 +0700

    upload file css to stylesheet

--- mayan/apps/appearance/forms.py
+++ mayan/apps/appearance/forms.py
@@ -6,10 +6,30 @@ from .models import Theme, UserThemeSetting
 
 
 class ThemeForm(forms.ModelForm):
-    class Meta:
-        fields = ('label','defaulttheme','fontname','stylesheet', 'logo',)
-        model = Theme
+    file_data = forms.FileField(required=False)
 
+    class Meta:
+        fields = ('label','defaulttheme','file_data','fontname','stylesheet', 'logo',)
+        model = Theme
+        widgets = {
+            'file_data': forms.FileInput(
+                attrs={
+                    'accept': '.css, .xml'
+                }
+            )
+        }
+
+    def save(self, commit=True):
+        obj = super().save(commit=False)
+
+        uploaded_file = self.cleaned_data['file_data']
+        if uploaded_file is not None:
+            obj.stylesheet = uploaded_file.read().decode()
+
+        if commit:
+            obj.save()
+
+        return obj
 
 class UserThemeSettingForm(forms.ModelForm):
     class Meta:
